d00432d7197bf3a50e9d355354c0263d3ec30585
SparshSingh10 <sparsh262002@gmail.com>
2023-11-25 22:07:05 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-const App = () => {
-  return <div>Hello world!</div>;
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+const App = ()=>{
+  return <Cart/>;
 };
-
 export default App;
